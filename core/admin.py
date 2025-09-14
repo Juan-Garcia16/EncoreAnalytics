687783b014d1b6964cb2d06ca8517d0f22090615
Juan Garcia <juan.garcia16@utp.edu.co>
2025-09-14 16:28:20 -0500

    Creacion de modelos en la app core. Registro de los modelos en el panel de admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import City, Artist, Venue
 
 # Register your models here.
+from django.contrib import admin
+from .models import Artist, City, Venue
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'debut_year', 'genre')
+    search_fields = ('name', 'genre')
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+    search_fields = ('name',)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'capacity')
+    search_fields = ('name',)
