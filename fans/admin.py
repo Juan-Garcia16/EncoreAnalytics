991195c2f206847fb2a4686f3670ea776dcaeb2e
Juan Garcia <juan.garcia16@utp.edu.co>
2025-09-14 18:18:35 -0500

    fans models + admin + migrations

--- fans/admin.py
+++ fans/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Fan, Attendance, Interest
 
 # Register your models here.
+@admin.register(Fan)
+class FanAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'city', 'birthdate')
+    search_fields = ('full_name', 'email')
+    list_filter = ('city',)
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('fan', 'concert', 'rating')
+    search_fields = ('fan__full_name', 'concert__artist__name', 'concert__venue__name')
+    list_filter = ('concert',)
+
+@admin.register(Interest)
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ('fan', 'concert', 'created_at')
+    search_fields = ('fan__full_name', 'concert__artist__name', 'concert__venue__name')
+    list_filter = ('created_at',)
