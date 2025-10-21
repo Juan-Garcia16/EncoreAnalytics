c04b4ac124e43bc309fe9de31b48abc4f981f8d0
Juan Garcia <juan.garcia16@utp.edu.co>
2025-10-21 11:37:00 -0500

    Reinicio proyecto

--- fans/urls.py
+++ fans/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.FanListView.as_view(), name='fan_list'),
     path('add/', views.FanCreateView.as_view(), name='fan_add'),
+    path('attendance/', views.AttendanceListView.as_view(), name='attendance_list'),
+    path('attendance/add/', views.AttendanceCreateView.as_view(), name='attendance_add'),
+    path('interest/', views.InterestListView.as_view(), name='interest_list'),
+    path('interest/add/', views.InterestCreateView.as_view(), name='interest_add'),
 ]
